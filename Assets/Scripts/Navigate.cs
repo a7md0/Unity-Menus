9ed04e87a4876db9f300e5d368abd41787ae9545
a7md0 <a7m3d699@gmail.com>
2021-03-10 13:00:10 +0300

    Create Navigate script

---
+++ Assets/Scripts/Navigate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigate : MonoBehaviour {
+    public void goToScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void exitGame() {
+        Application.Quit();
+    }
+}
